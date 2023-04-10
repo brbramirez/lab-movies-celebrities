d04dec3cf909e5416f4108321ae1c9b40dd3f221
Barbara Ramirez <brcmua@gmail.com>
2023-04-10 20:14:44 +0200

    done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -23,6 +23,43 @@ router.get("/celebrities", async (req, res, next) => {
     res.render('celebrities/celebrities', { allCelebrities })
 });
 
+//Celebrities details
+router.get("/celebrities/:id", async (req,res) => {
+  const celebrityId = req.params.id;
+  const celebrityDetails = await celebrity.findById(celebrityId);
+  res.render('celebrities/celebrities-details', {celebrityDetails});
+})
 
+//Delete a celebrity
+router.post("/celebrities/:id/delete", async (req, res) => {
+  try {
+    await celebrity.findByIdAndDelete(req.params.id);
+    const allCelebrities = await celebrity.find();
+    res.render('celebrities/celebrities', {allCelebrities});
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//Edit a celebrity
+//Rendering the form
+router.get("/celebrities/:id/edit", (req, res) => {
+  celebrity.findById(req.params.id)
+    .then((celebrityFromDB) => {
+      res.render("celebrities/edit-celebrity", { celebrity: celebrityFromDB });
+    })
+    .catch((err) => console.log(err));
+});
+//Posting the info
+router.post("/celebrities/:id/edit", async (req, res) => {
+  try {
+    const editedCelebrity = req.body;
+    await celebrity.findByIdAndUpdate(req.params.id, editedCelebrity)
+    const allCelebrities = await celebrity.find();
+    res.render('celebrities/celebrities', {allCelebrities});
+  } catch (err) {
+    console.log(err);
+  }
+})
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -4,11 +4,15 @@ const movie = require("../models/Movie.model");
 const celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+
+//Create a new movie
+//Rendering the form with the actual values
 router.get("/movies/create", async (req, res, next) => {
     const allCelebrities = await celebrity.find();
     res.render('movies/new-movie', { allCelebrities })
 });
 
+//Posting the info
 router.post("/movies/create", async (req, res) => {
     try {
       const newMovie = req.body;
@@ -19,18 +23,22 @@ router.post("/movies/create", async (req, res) => {
     }
   });
 
+  //See all movies
   router.get("/movies", async (req, res, next) => {
     const allMovies = await movie.find();
     console.log(allMovies);
     res.render('movies/movies', { allMovies })
 });
 
+//Movie details
 router.get("/movies/:id", async (req,res) => {
     const movieId = req.params.id;
     const moviePlot = await movie.findById(movieId).populate('cast');
     res.render('movies/movie-details', {moviePlot});
 })
 
+//Edit a movie
+//Rendering the form
 router.get("/movies/:id/edit", async (req,res) => {
     try {
         const movieId = req.params.id;
@@ -42,7 +50,7 @@ router.get("/movies/:id/edit", async (req,res) => {
       }
     
 })
-
+//Posting the info
 router.post("/movies/:id/edit", (req, res) => {
     const editedMovie = req.body;
     movie.findByIdAndUpdate(req.params.id, editedMovie)
